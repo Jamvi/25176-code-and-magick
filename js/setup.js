a213e0c0405a556077ebb1d0fc61741842edcc40
Egor Tarakanov <Egor_Tarakanov@epam.com>
2018-02-07 00:48:06 +0300

    Добавлена проверка на исключение полных тезок; убраны циклы из глобальной области видимости

--- js/setup.js
+++ js/setup.js
@@ -3,36 +3,48 @@
 var userDialog = document.querySelector('.setup');
 userDialog.classList.remove('hidden');
 
-var similarListElement = document.querySelector('.setup-similar-list');
-var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
+var SIMILAR_LIST_ELEMENT = document.querySelector('.setup-similar-list');
+var SIMILAR_WIZARD_TEMPLATE = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
 
-var firstNames = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
-var lastNames = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
-var coatColors = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
-var eyesColors = ['black', 'red', 'blue', 'yellow', 'green'];
+var FIRST_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+var LAST_NAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
 
-var wizards = [];
+var createUniqueName = function (names, surnames, duplicateNames) {
+
+  var fullName = names[Math.floor(Math.random() * names.length)] + ' ' + surnames[Math.floor(Math.random() * surnames.length)];
+
+  if (duplicateNames.indexOf(fullName) > -1) {
+    fullName = createUniqueName(names, surnames, duplicateNames);
+  } else {
+    duplicateNames.push(fullName);
+  }
+
+  return fullName;
+};
 
 var createWizard = function (names, surnames, coats, eyes) {
 
-  var firstName = names.splice(Math.floor(Math.random() * names.length), 1);
-  var lastName = surnames.splice(Math.floor(Math.random() * surnames.length), 1);
+  var duplicateFullNames = [];
+  var wizardsCatalog = [];
 
-  var wizard = {
-    name: firstName + ' ' + lastName,
-    coatColor: coats[Math.floor(Math.random() * coats.length)],
-    eyesColor: eyes[Math.floor(Math.random() * eyes.length)]
-  };
+  for (var i = 0; i < 4; i++) {
 
-  return wizard;
+    var wizard = {
+      name: createUniqueName(names, surnames, duplicateFullNames),
+      coatColor: coats[Math.floor(Math.random() * coats.length)],
+      eyesColor: eyes[Math.floor(Math.random() * eyes.length)]
+    };
+    wizardsCatalog[i] = wizard;
+
+  }
+
+  return wizardsCatalog;
 };
 
-for (var i = 0; i < 4; i++) {
-  wizards[i] = createWizard(firstNames, lastNames, coatColors, eyesColors);
-}
-
 var renderWizard = function (wizard) {
-  var wizardElement = similarWizardTemplate.cloneNode(true);
+  var wizardElement = SIMILAR_WIZARD_TEMPLATE.cloneNode(true);
 
   wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
   wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
@@ -41,12 +53,18 @@ var renderWizard = function (wizard) {
   return wizardElement;
 };
 
-var fragment = document.createDocumentFragment();
+var renderWizards = function (wizardsList) {
+  var fragment = document.createDocumentFragment();
 
-for (var j = 0; j < wizards.length; j++) {
-  fragment.appendChild(renderWizard(wizards[j]));
-}
+  for (var j = 0; j < wizards.length; j++) {
+    fragment.appendChild(renderWizard(wizardsList[j]));
+  }
 
-similarListElement.appendChild(fragment);
+  SIMILAR_LIST_ELEMENT.appendChild(fragment);
+};
+
+var wizards = createWizard(FIRST_NAMES, LAST_NAMES, COAT_COLORS, EYES_COLORS);
+
+renderWizards(wizards);
 
 document.querySelector('.setup-similar').classList.remove('hidden');
