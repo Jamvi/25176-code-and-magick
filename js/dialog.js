89e26870fe6400c7a06c240e6ae149e67da8b88d
Egor Tarakanov <Egor_Tarakanov@epam.com>
2018-02-22 02:02:41 +0300

    Реализовано перетаскивание элементов

--- js/dialog.js
+++ js/dialog.js
@@ -1,23 +1,27 @@
 'use strict';
 
 (function () {
-  var setupModal = document.querySelector('.setup');
-  var setupModalOpen = document.querySelector('.setup-open');
+  var setup = document.querySelector('.setup');
+  var setupOpen = document.querySelector('.setup-open');
   var setupOpenIcon = document.querySelector('.setup-open-icon');
-  var setupModalClose = setupModal.querySelector('.setup-close');
-  var userNameInput = setupModal.querySelector('.setup-user-name');
+  var setupClose = setup.querySelector('.setup-close');
+  var userNameInput = setup.querySelector('.setup-user-name');
+  var dialogHandle = setup.querySelector('.upload');
+  var setupTop = getComputedStyle(setup).getPropertyValue('top');
+  var setupLeft = getComputedStyle(setup).getPropertyValue('left');
 
   var openPopup = function () {
-    setupModal.classList.remove('hidden');
+    setup.classList.remove('hidden');
     document.addEventListener('keydown', onPopupEscPress);
   };
 
   var closePopup = function () {
-    setupModal.classList.add('hidden');
+    setup.classList.add('hidden');
+    returnPopupPosition();
     document.removeEventListener('keydown', onPopupEscPress);
   };
 
-  setupModalOpen.addEventListener('click', openPopup);
+  setupOpen.addEventListener('click', openPopup);
 
   var onPopupEscPress = function (evt) {
     window.util.isEscEvent(evt, closePopup);
@@ -27,9 +31,9 @@
     window.util.isEnterEvent(evt, openPopup);
   });
 
-  setupModalClose.addEventListener('click', closePopup);
+  setupClose.addEventListener('click', closePopup);
 
-  setupModalClose.addEventListener('keydown', function (evt) {
+  setupClose.addEventListener('keydown', function (evt) {
     window.util.isEnterEvent(evt, closePopup);
   });
 
@@ -43,6 +47,69 @@
     window.util.isEnterEvent(evt, function () {
       evt.preventDefault();
     });
+  });
+
+  var returnPopupPosition = function () {
+    setup.style.top = setupTop;
+    setup.style.left = setupLeft;
+  };
+
+  dialogHandle.addEventListener('mousedown', function (evt) {
+    evt.preventDefault();
+
+    var startCoords = {
+      x: evt.clientX,
+      y: evt.clientY
+    };
+
+    var borders = {
+      minX: setup.offsetWidth / 2,
+      maxX: window.innerWidth - setup.offsetWidth / 2 - dialogHandle.offsetLeft,
+      minY: 0,
+      maxY: window.innerHeight
+    };
+
+    var dragged = false;
+
+    var onMouseMove = function (moveEvt) {
+      moveEvt.preventDefault();
+      dragged = true;
+
+      var shift = {
+        x: startCoords.x - moveEvt.clientX,
+        y: startCoords.y - moveEvt.clientY
+      };
+
+      startCoords = {
+        x: moveEvt.clientX,
+        y: moveEvt.clientY
+      };
+
+      var dialogX = Math.min(Math.max((setup.offsetLeft - shift.x), borders.minX), borders.maxX);
+      var dialogY = Math.min(Math.max((setup.offsetTop - shift.y), borders.minY), borders.maxY);
+
+      setup.style.left = dialogX + 'px';
+      setup.style.top = dialogY + 'px';
+
+    };
+
+    var onMouseUp = function (upEvt) {
+      upEvt.preventDefault();
+
+      document.removeEventListener('mousemove', onMouseMove);
+      document.removeEventListener('mouseup', onMouseUp);
+
+      if (dragged) {
+        var onClickPreventDefault = function (evt) {
+          evt.preventDefault();
+          dialogHandle.removeEventListener('click', onClickPreventDefault);
+        };
+        dialogHandle.addEventListener('click', onClickPreventDefault);
+      }
+    };
+
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseup', onMouseUp);
   });
 
 })();
